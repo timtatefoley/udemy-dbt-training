ce1aee1c748350e340d3aa4f6a8d3cc43336cbae
Tim Foley <tim@activatecare.com>
2023-04-27 15:17:05 +0000

    Developing stg_orders,  with all three raw tables,  and added a profit calculation

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select 
+-- COMMENT:  from raw orders
+ord.orderid,
+ord.orderdate,
+ord.shipdate,
+ord.shipmode,
+ord.ordersellingprice,
+ord.ordercostprice,
+ord.ordersellingprice - ord.ordercostprice as orderprofit,
+-- COMMENT:  from raw customer
+cu.customername,
+cu.segment,
+cu.country,
+-- COMMENT:  from raw product
+pr.category,
+pr.productname,
+pr.subcategory
+from {{ ref('raw_orders') }} as ord
+left join {{ ref('raw_customer') }} as cu
+on ord.customerid = cu.customerid
+left join {{ ref('raw_product') }} as pr 
+on ord.productid = pr.productid
