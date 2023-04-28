fbe1081a0f4624c02bbe7f27f862693c5a309607
Tim Foley <tim@activatecare.com>
2023-04-28 21:32:59 +0000

    created delivery team seed and added delivery team seed to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select 
 -- COMMENT:  from raw orders
+{{ dbt_utils.generate_surrogate_key(['ord.orderid', 'cu.customerid','pr.productid']) }} as sk_orders,
 ord.orderid,
 ord.orderdate,
 ord.shipdate,
@@ -17,9 +18,12 @@ pr.productid,
 pr.category,
 pr.productname,
 pr.subcategory,
-{{ markup('ordercostprice2ordersellingprice','ordercostprice') }} as markup
+{{ markup('ordercostprice2ordersellingprice','ordercostprice') }} as markup,
+de.delivery_team
 from {{ ref('raw_orders') }} as ord
 left join {{ ref('raw_customer') }} as cu
 on ord.customerid = cu.customerid
 left join {{ ref('raw_product') }} as pr 
 on ord.productid = pr.productid
+left join {{ ref('delivery_team') }} as de
+on ord.shipmode = de.shipmode
