59316c0d6feb305a8082d0adb91219f9ad01b8ee
Tim Foley <tim@activatecare.com>
2023-04-28 20:40:49 +0000

    Many small changes / edits

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -4,19 +4,20 @@ ord.orderid,
 ord.orderdate,
 ord.shipdate,
 ord.shipmode,
-ord.productid,
-ord.ordercostprice2ordersellingprice,
-ord.ordercostprice,
 ord.ordercostprice2ordersellingprice - ord.ordercostprice as orderprofit,
+ord.ordercostprice,
+ord.ordercostprice2ordersellingprice,
 -- COMMENT:  from raw customer
 cu.customerid,
 cu.customername,
 cu.segment,
 cu.country,
 -- COMMENT:  from raw product
+pr.productid,
 pr.category,
 pr.productname,
-pr.subcategory
+pr.subcategory,
+{{ markup('ordercostprice2ordersellingprice','ordercostprice') }} as markup
 from {{ ref('raw_orders') }} as ord
 left join {{ ref('raw_customer') }} as cu
 on ord.customerid = cu.customerid
